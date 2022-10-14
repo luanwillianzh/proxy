93d4f6feeec4d31a43844f1239c4e4b5e26b185d
luanwillianzh <74968256+luanwillianzh@users.noreply.github.com>
2022-10-14 15:25:41 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 import NextCors  from 'nextjs-cors';
 
 const apiProxy = createProxyMiddleware({
-  target: "https://api.pinboard.in",
+  target: "https://spotify.luanwillian.repl.co",
   changeOrigin: true,
   pathRewrite: {
     "^/api": "" // Strip "/api" from the URL 
